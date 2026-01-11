1f5856eb5a966032e75cb38d6bdc68c0403db825
Santiago-Santiago <santiagojsantiago.04@gmail.com>
2026-01-11 08:47:37 -0500

    feat: add NavigationProvider and wrap NewAppScreen in it

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { View, Text, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { NavigationProvider } from './providers/NavigationProvider';
 
 export default function AppScreen() {
     const safeAreaInsets = useSafeAreaInsets();
     return (
-        <NewAppScreen
-            templateFileName="App.tsx"
-            safeAreaInsets={safeAreaInsets}
-        />
-        // <View style={styles.container}>
-        //     <Text>Enterprise RN Starter</Text>
-        // </View>
+        <NavigationProvider>
+            <NewAppScreen
+                templateFileName="App.tsx"
+                safeAreaInsets={safeAreaInsets}
+            />
+        </NavigationProvider>
     );
 }
 
---
+++ src/app/providers/NavigationProvider.tsx
@@ -0,0 +1,5 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+export function NavigationProvider({ children }: { children: React.ReactNode }) {
+    return <NavigationContainer>{children}</NavigationContainer>
+}
